b1334ee7a335446e83b5557386fd0df928ff23c9
Diego Beltran <dbcaturra@gmail.com>
2019-10-19 15:13:58 -0500

    update

---
+++ app/src/main/java/co/miescuela/miescuela/LmsActivity.java
@@ -0,0 +1,83 @@
+package co.miescuela.miescuela;
+//package co.miescuela;
+
+import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+
+/** Basic embedded browser for viewing LMS pages. */
+public final class LmsActivity extends Activity {
+
+    WebView mWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        final RelativeLayout progressLayout = new RelativeLayout(this);
+        RelativeLayout.LayoutParams lParams = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        lParams.addRule(RelativeLayout.CENTER_IN_PARENT);
+        ProgressBar progressBar = new ProgressBar(this);
+        progressBar.setIndeterminate(true);
+        progressBar.setLayoutParams(lParams);
+        progressLayout.addView(progressBar);
+
+        mWebView = new WebView(this);
+        WebSettings settings = mWebView.getSettings();
+
+
+        // Enable Javascript
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setCacheMode(WebSettings.LOAD_DEFAULT);
+        mWebView.setWebViewClient(new WebViewClient());
+        setContentView(progressLayout);
+        mWebView.clearCache(true);
+        mWebView.loadUrl("http://192.168.43.1:8080");
+
+        mWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                if (url.startsWith("http://192.168.43.1:8080")) {
+                    // Inline help.
+                    setContentView(progressLayout);
+                    return false;
+                }
+
+                try {
+                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(url)).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+                } catch (ActivityNotFoundException e) {
+                    // Android TV does not have a system browser.
+                    setContentView(progressLayout);
+                    return false;
+                }
+                return true;
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                setContentView(mWebView);
+            }
+        });
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mWebView.canGoBack()) {
+            mWebView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+}
+
